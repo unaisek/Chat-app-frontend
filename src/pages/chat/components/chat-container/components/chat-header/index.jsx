bac21ef81c8dc260e5b9047a495a8ea1a4e7b62c
unaisek <mohammedunaiseknkt@gmail.com>
2024-12-05 22:40:40 +0530

    chat layout completed

---
+++ src/pages/chat/components/chat-container/components/chat-header/index.jsx
@@ -0,0 +1,18 @@
+import { RiCloseFill  } from 'react-icons/ri'
+const ChatHeader = () => {
+  return (
+    <div className="h-[10vh] border-b-2 border-[#2f303b] flex items-center justify-between">
+      <div className="flex items-center gap-5">
+        <div className="flex gap-3 items-center justify-center"></div>
+        <div className="flex items-center justify-center gap-5">
+          <button className='focus:border-none text-neutral-500 focus:outline-none focus:text-white duration-300 transition-all'>
+            <RiCloseFill className='text-3xl' />
+          </button>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ChatHeader
--- src/pages/chat/index.jsx
+++ src/pages/chat/index.jsx
@@ -2,13 +2,26 @@ import { useAppStore } from "@/store"
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import { useEffect } from "react";
+import ChatContainer from "./components/chat-container";
+import ContactContainer from "./components/contact-container";
+import EmptyChatContainer from "./components/empty-container";
 
 export const Chat = ()=>{
   const { userInfo } = useAppStore();
   const navigate = useNavigate();
-
+  
+  useEffect (() => {
+    if(!userInfo.profileSetup){
+      toast("Please setup your profile")
+      navigate("/profile")
+    }
+  },[userInfo, navigate])
   
   return (
-    <div>Chat</div>
+    <div className="flex h-[100vh] text-white overflow-hidden">
+      <ContactContainer />
+      <ChatContainer />
+      <EmptyChatContainer />
+    </div>
   )
 }
